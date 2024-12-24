4c1e60b987306156f4023dd5ac69c62b72e4bbf1
BKbud <tsb0704@naver.com>
2024-12-24 16:04:51 +0900

    del dirs

--- Web/Javascript/todo.js
+++ Web/Javascript/todo.js
@@ -7,17 +7,31 @@
 // console.log("todo.js");
 
 function addTodo(text) {
+    // 문자열 보간법
     console.log(`addTodo: ${text}`); // `${}` -> 문자열 보간법
-    // 동적으로 리스트를 추가할 수 있도록 list 객체를 받아와 사용.
     const todoList = document.getElementById('todoList');
-    const li = document.createElement('li'); // <li></li>
-    li.textContent = text; // <li>text</li>
-    todoList.appendChild(li); // <ul><li>text</li></ul>
-}
-
-// '추가' 버튼이 눌렸을 때의 이벤트 동작 정의
-document.getElementById('addTodo').addEventListener('click', () => {
+    // 동적으로 리스트를 추가할 수 있도록 li 태그를 생성
+    const li = document.createElement('li'); // <li></li> 생성
+    li.textContent = text; // <li>text</li> 내용 추가
+    li.classList.add('list-group-item'); // <li class="list-group-item">text</li> 클래스 추가
+    // 체크박스 추가
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('form-check-input');
+  
+    checkbox.addEventListener('change', () => {
+      console.log('checkbox change');
+      // 체크박스 체크 여부에 따라 취소선 추가
+      li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+    });
+    
+    li.prepend(checkbox); // prepend: 앞에 추가
+    todoList.append(li); // <ul><li class="list-group-item">text</li></ul> 추가
+  }
+  
+  // '추가' 버튼이 눌렸을 때의 이벤트 동작 정의
+  document.getElementById('addTodo').addEventListener('click', () => {
     const input = document.getElementById('todoInput');
     addTodo(input.value);
     input.value = '';
-})
+  });
