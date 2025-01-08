216884eb28c2fbb5f205da8f916ef05b81e1eedb
BKbud <tsb07040@gmail.com>
2025-01-08 16:08:11 +0900

    swiftUI helloworld

---
+++ swift/swiftUIProgramming/05.DataType/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 정보경 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
