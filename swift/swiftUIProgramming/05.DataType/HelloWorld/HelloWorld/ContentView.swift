c295ca167fb5a1ec467aaefd9e89e54d558cf332
BKbud <tsb07040@gmail.com>
2025-01-09 14:35:53 +0900

    duplicated label

--- swift/swiftUIProgramming/05.DataType/HelloWorld/HelloWorld/ContentView.swift
+++ swift/swiftUIProgramming/05.DataType/HelloWorld/HelloWorld/ContentView.swift
@@ -55,68 +55,121 @@ import SwiftUI
 //    }
 //}
 
+// ----------custom label-----------
+//// View 프로토콜을 따르는 Generic type 'MyContent'
+//struct MyVStack<MyContent: View>: View {
+//    let content: () -> MyContent
+//
+//    // @escaping: 현재 함수 범위를 넘어가도 실행될 수 있도록 하는 어노테이션
+//    // @ViewBuilder: 클로저 문법으로 받은 파라미터가 뷰라는 것을 명시적으로 표기하는 속성
+//    // 스위프트 클로저 형태를 취하며 여러 하위 뷰로 구성된 커스텀 뷰를 만드는 데 사용될 수 있다.
+//    init(@ViewBuilder content: @escaping () -> MyContent) {
+//        self.content = content
+//    }
+//
+//    var body: some View {
+//        VStack(spacing: 10) {
+//            content()
+//        }
+//        .font(.title)
+//    }
+//}
+//
+//// 커스텀 레이블 스타일 (레이아웃)
+//struct VerticalLabelStyle: LabelStyle {
+//    func makeBody(configuration: Configuration) -> some View {
+//        VStack(alignment: .center) {
+//            configuration.icon
+//                .padding(5)
+//            configuration.title
+//        }
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        MyVStack (content: {
+//            Text("Text1")
+//            Text("Text2")
+//            HStack {
+//                Image(systemName: "star.fill")
+//                Image(systemName: "star.fill")
+//                Image(systemName: "star")
+//            }
+//            Button(action:{
+//                print("click")
+//            }) {
+//                Label("Welcome to SwiftUI",
+//                      systemImage: "person.circle.fill")
+//            }
+//            Label(
+//                title: {
+//                    Text("Welcome to SwiftUI")
+//                        .font(.largeTitle)
+//                }, icon: {
+//                    Circle()
+//                        .fill(Color.blue)
+//                        .frame(width: 25, height: 25)
+//                }
+//            )
+//            .labelStyle(.titleAndIcon)
+//            Label("수직 레이블", systemImage: "moon.fill")
+//                .labelStyle(VerticalLabelStyle())
+//        })
+//    }
+//}
 
-struct MyVStack<MyContent: View>: View {
-    let content: () -> MyContent
-    
-    // @escaping: 현재 함수 범위를 넘어가도 실행될 수 있도록 하는 어노테이션
-    // @ViewBuilder: 클로저 문법으로 받은 파라미터가 뷰라는 것을 명시적으로 표기하는 속성
-    // 스위프트 클로저 형태를 취하며 여러 하위 뷰로 구성된 커스텀 뷰를 만드는 데 사용될 수 있다. 
-    init(@ViewBuilder content: @escaping () -> MyContent) {
-        self.content = content
-    }
-    
-    var body: some View {
-        VStack(spacing: 10) {
-            content()
-        }
-        .font(.title)
-    }
-}
-
-// 커스텀 레이블 스타일 (레이아웃)
-struct VerticalLabelStyle: LabelStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        VStack(alignment: .center) {
-            configuration.icon
-                .padding(5)
-            configuration.title
-        }
-    }
-}
-
+//-------------duplicated label------------
 struct ContentView: View {
     var body: some View {
-        MyVStack (content: {
-            Text("Text1")
-            Text("Text2")
+        VStack {
             HStack {
-                Image(systemName: "star.fill")
-                Image(systemName: "star.fill")
-                Image(systemName: "star")
+                Image(systemName: "goforward.10")
+                Image(systemName: "goforward.15")
+                Image(systemName: "goforward.30")
             }
-            Button(action:{
-                print("click")
-            })
-            {
-                Label("Welcome to SwiftUI",
-                      systemImage: "person.circle.fill")
+            ZStack {
+                Image(systemName: "goforward.10")
+                Image(systemName: "goforward.15")
+                Image(systemName: "goforward.30")
             }
-            Label(
-                title: {
-                    Text("Welcome to SwiftUI")
-                        .font(.largeTitle)
-                }, icon: {
-                    Circle()
-                        .fill(Color.blue)
-                        .frame(width: 25, height: 25)
+            // 복잡한 중첩 레이아웃 예시
+            Text("Financial Results")
+                .font(.title)
+            HStack {
+                Text("Q1 Sales")
+                    .font(.headline)
+                VStack {
+                    Text("January")
+                    Text("February")
+                    Text("March")
                 }
-            )
-            .labelStyle(.titleAndIcon)
-            Label("수직 레이블", systemImage: "moon.fill")
-                .labelStyle(VerticalLabelStyle())
-        })
+                VStack {
+                    Text("$1000")
+                    Text("$200")
+                    Text("$3000")
+                }
+            }
+            // 정렬, 여백
+            HStack(alignment: .top) {
+                Text("Q1 Sales")
+                    .font(.headline)
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("January")
+                    Text("February")
+                    Text("March")
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("$1000")
+                    Text("$200")
+                    Text("$3000")
+                }
+            }
+        }
     }
+    
 }
 
 #Preview {
