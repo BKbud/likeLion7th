89f2ad130c1c74b1b0da27d78f5ad3d800286d3e
BKbud <tsb07040@gmail.com>
2025-01-09 13:01:52 +0900

    count app

--- swift/swiftUIProgramming/05.DataType/HelloWorld/HelloWorld/ContentView.swift
+++ swift/swiftUIProgramming/05.DataType/HelloWorld/HelloWorld/ContentView.swift
@@ -8,18 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    //상태를 관리하는 프로퍼티
+    @State private var count = 0
+    
     var body: some View {
-        VStack {
-            Text("Text1")
-            Text("Text2")
-            Text("Text3")
+        VStack(spacing: 20) {
+            Text("Count")
+                .font(.largeTitle)
+            
+            Text("\(count)")
+                .font(.system(size: 100))
+                .fontWeight(.bold)
+                .foregroundColor(count % 2 == 0 ? .blue : .red)
             HStack {
-                Text("Text4")
-                Text("Text5")
+                Button(action: {
+                    self.count -= 1
+                }, label: {
+                    Text("-")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundStyle(.white)
+                        .clipShape(Circle())
+                })
+                Spacer()
+                
+                Button("RESET", action: {
+                    self.count = 0
+                })
+                .font(.largeTitle)
+                Spacer()
+                
+                Button(action: {
+                    self.count += 1
+                }, label: {
+                    Text("+")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundStyle(.white)
+                        .clipShape(Circle())
+                })
             }
-            Text("Text6")
-            Text("Hello") + Text(", ") + Text("World!")
         }
+        .padding(40)
     }
 }
 
