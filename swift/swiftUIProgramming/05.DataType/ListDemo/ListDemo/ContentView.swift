a6e7d3cdd7ace521ff58032d46b71ad3a8a09047
BKbud <tsb07040@gmail.com>
2025-01-14 13:07:27 +0900

    dynamic list & refreshable

--- swift/swiftUIProgramming/05.DataType/ListDemo/ListDemo/ContentView.swift
+++ swift/swiftUIProgramming/05.DataType/ListDemo/ListDemo/ContentView.swift
@@ -5,33 +5,47 @@
 //  Created by 정보경 on 1/14/25.
 //
 
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var task: String
+    var imageName: String
+}
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var listData: [ToDoItem] = [
+        ToDoItem(task: "Take out trash", imageName: "trash.circle.fill"),
+        ToDoItem(task: "Pick up the kids", imageName: "person.2.fill"),
+        ToDoItem(task: "Wash the car", imageName: "car.fill")
+    ]
+    
+    @State private var toggleStatus = true
+    
     var body: some View {
+        
         List {
-            HStack {
-                Image(systemName: "trash.circle.fill")
-                Text("Take out the trash")
+            Section(header: Text("Settings")) {
+                Toggle(isOn: $toggleStatus) {
+                    Text("Allow notifications")
+                }
             }
-            HStack {
-                Image(systemName: "person.2.fill")
-                Text("Pick up the kids")
-            }
-            HStack {
-                Image(systemName: "car.fill")
-                Text("Wash the car")
+            Section(header: Text("To Do Task")) {
+                ForEach (listData) { item in
+                    HStack {
+                        Image(systemName: item.imageName)
+                        Text(item.task)
+                    }
+                }
             }
         }
-        List {
-            Text("Wash the car")
-                .listRowSeparator(.hidden)
-            Text("Pick up kids from school bus @ 3pm")
-                .listRowSeparatorTint(.green)
-            Text("Auction the kids on eBay")
-                .listRowSeparatorTint(.red)
-            Text("Order Pizza for dinner")
-                .listRowBackground(Image("images"))
+        .refreshable {
+            listData = [
+                ToDoItem(task: "Order dinner", imageName: "dollarsign.circle.fill"),
+                ToDoItem(task: "Order lunch", imageName: "dollarsign.circle.fill"),
+                ToDoItem(task: "Order breakfast", imageName: "dollarsign.circle.fill"),
+            ]
         }
     }
 }
