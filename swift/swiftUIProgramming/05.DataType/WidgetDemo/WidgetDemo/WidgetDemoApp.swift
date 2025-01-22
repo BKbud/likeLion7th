0a58ef00b05fa7ad5591be41784994f209ec1e51
BKbud <tsb07040@gmail.com>
2025-01-22 17:24:07 +0900

    widget first commit

---
+++ swift/swiftUIProgramming/05.DataType/WidgetDemo/WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by 정보경 on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
