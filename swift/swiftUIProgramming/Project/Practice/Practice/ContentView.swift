3c94668bffc3d6b99a88aff0abb6f8cc19a9a3e1
BKbud <tsb07040@gmail.com>
2025-01-27 23:08:41 +0900

    initial commit

---
+++ swift/swiftUIProgramming/Project/Practice/Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice
+//
+//  Created by 정보경 on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/swiftUIProgramming/Project/Practice/Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by 정보경 on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
