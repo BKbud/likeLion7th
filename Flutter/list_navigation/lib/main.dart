1f92a979df16bcc2e7ebaff934e5e2d2f9f21751
BKbud <tsb07040@gmail.com>
2025-02-13 15:41:45 +0900

    navigation

--- Flutter/list_navigation/lib/main.dart
+++ Flutter/list_navigation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:list_navigation/my_list_view.dart';
 import 'my_sliver_list_view.dart';
 import 'my_grid_view.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyGridView(),
+      home: const MyListView(),
     );
   }
 }
--- Flutter/list_navigation/lib/my_list_view.dart
+++ Flutter/list_navigation/lib/my_list_view.dart
@@ -32,9 +32,34 @@ class MyListView extends StatelessWidget {
         itemCount: item.monthItems.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(item.monthItems[index]),
-          );
+              title: Text(item.monthItems[index]),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            MyDetails(item.monthItems[index])));
+              });
         },
+        itemExtent: 50,
+      ),
+    );
+  }
+}
+
+class MyDetails extends StatelessWidget {
+  final String month;
+  const MyDetails(this.month, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(month),
+      ),
+      body: Center(
+        child: Text('This is the details page for $month'),
       ),
     );
   }
