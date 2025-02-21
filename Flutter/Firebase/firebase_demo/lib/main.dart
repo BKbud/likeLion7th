4f39bc2ea6f5f67504ef9846704b5bf9ca3941c6
BKbud <tsb07040@gmail.com>
2025-02-21 10:13:04 +0900

    인증 기능 추가, 에뮬레이터 설정

--- Flutter/Firebase/firebase_demo/lib/main.dart
+++ Flutter/Firebase/firebase_demo/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,8 +8,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  // Firebase Firestore Emulator 사용
-  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  if (kDebugMode) {
+    // Firebase Firestore Emulator 사용
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    FirebaseAuth.instance.useAuthEmulator('localhost', 9090);
+  }
 
   runApp(const MyApp());
 }
