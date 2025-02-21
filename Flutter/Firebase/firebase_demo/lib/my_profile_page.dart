57715ec94222e3bcfcdf9c26ea2941e82e3ca7f5
BKbud <tsb07040@gmail.com>
2025-02-21 15:17:30 +0900

    image picker 추가, 이미지 권한 설정 추가

--- Flutter/Firebase/firebase_demo/lib/my_profile_page.dart
+++ Flutter/Firebase/firebase_demo/lib/my_profile_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyProfilePage extends StatefulWidget {
   const MyProfilePage({super.key});
@@ -15,6 +19,37 @@ class _MyProfilePageState extends State<MyProfilePage> {
   void initState() {
     super.initState();
     _profileImageUrl = FirebaseAuth.instance.currentUser?.photoURL;
+  }
+
+  Future<void> _updateProfileImage() async {
+    try {
+      final XFile? pickedImage = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+      );
+
+      if (pickedImage == null) {
+        return;
+      }
+
+      // 파일 업로드
+      final File file = File(pickedImage.path);
+      final ref = FirebaseStorage.instance.ref(
+        'profile_images/${FirebaseAuth.instance.currentUser!.uid}.jpg',
+      );
+      await ref.putFile(file);
+
+      // 다운로드 URL 업데이트
+      final String downloadUrl = await ref.getDownloadURL();
+
+      // 사용자 프로필 업데이트
+      await FirebaseAuth.instance.currentUser?.updatePhotoURL(downloadUrl);
+
+      setState(() {
+        _profileImageUrl = downloadUrl;
+      });
+    } catch (e) {
+      debugPrint('프로필 사진 변경 실패: $e');
+    }
   }
 
   @override
@@ -48,7 +83,7 @@ class _MyProfilePageState extends State<MyProfilePage> {
               color: Theme.of(context).colorScheme.primary,
               textColor: Colors.white,
               minWidth: 160,
-              onPressed: () {},
+              onPressed: _updateProfileImage,
               child: const Text('프로필 사진 변경'),
             ),
           ],
