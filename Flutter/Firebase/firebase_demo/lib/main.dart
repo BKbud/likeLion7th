93df6334221db678c1b506d4d8916b702a273986
BKbud <tsb07040@gmail.com>
2025-02-21 11:06:43 +0900

    firebase에 이메일, 비밀번호 연동

--- Flutter/Firebase/firebase_demo/lib/main.dart
+++ Flutter/Firebase/firebase_demo/lib/main.dart
@@ -26,6 +26,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontSize: 20.0),
+          bodyMedium: TextStyle(fontSize: 15.0),
+          bodySmall: TextStyle(fontSize: 10.0),
+          labelLarge: TextStyle(fontSize: 20.0),
+          labelMedium: TextStyle(fontSize: 15.0),
+          labelSmall: TextStyle(fontSize: 10.0),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const AuthScreen(),
@@ -44,6 +52,32 @@ class _AuthScreenState extends State<AuthScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  Future<void> _signUp() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: _emailController.text,
+            password: _passwordController.text,
+          );
+      debugPrint('회원가입 성공: ${userCredential.user}');
+    } catch (e) {
+      debugPrint('회원가입 실패: ${e.toString()}');
+    }
+  }
+
+  Future<void> _signIn() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+            email: _emailController.text,
+            password: _passwordController.text,
+          );
+      debugPrint('로그인 성공: ${userCredential.user}');
+    } catch (e) {
+      debugPrint('로그인 실패: ${e.toString()}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,10 +94,23 @@ class _AuthScreenState extends State<AuthScreen> {
             TextField(
               controller: _passwordController,
               decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
             ),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: const Text('로그인')),
-            ElevatedButton(onPressed: () {}, child: const Text('회원가입')),
+            MaterialButton(
+              color: Theme.of(context).colorScheme.primary,
+              textColor: Colors.white,
+              minWidth: double.infinity,
+              onPressed: _signIn,
+              child: const Text('로그인'),
+            ),
+            MaterialButton(
+              color: Theme.of(context).colorScheme.secondary,
+              textColor: Colors.white,
+              minWidth: double.infinity,
+              onPressed: _signUp,
+              child: const Text('회원가입'),
+            ),
           ],
         ),
       ),
