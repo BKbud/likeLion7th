404b292aaf9679cdde8a296f9cc94fd8a51050c8
BKbud <tsb07040@gmail.com>
2025-02-25 11:17:38 +0900

    gridView

--- Flutter/flutter/layout_demo/lib/main.dart
+++ Flutter/flutter/layout_demo/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:layout_demo/grid_screen.dart';
+import 'package:layout_demo/list_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  // runApp(ListScreen());
+  runApp(GridScreen());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +19,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Placeholder(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('반응형 UI 예제')),
+        body: Column(
+          children: [
+            LayoutBuilder(
+              builder: (context, BoxConstraints constraints) {
+                double fontSize = constraints.maxWidth > 600 ? 32 : 24;
+                return Center(
+                  child: Text('반응형 텍스트', style: TextStyle(fontSize: fontSize)),
+                );
+              },
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2, // 2:1 비율
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.red,
+                    child: Text('위젯 1'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    color: Colors.green,
+                    child: Text('위젯 2'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
