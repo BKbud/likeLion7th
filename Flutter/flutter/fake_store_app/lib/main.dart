970d169b8f25e3d8429f8eda9bd7586227426d07
BKbud <tsb07040@gmail.com>
2025-02-25 14:44:25 +0900

    product list screen

--- Flutter/flutter/fake_store_app/lib/main.dart
+++ Flutter/flutter/fake_store_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'views/product_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Placeholder(),
+      home: const ProductListScreen(),
     );
   }
 }
---
+++ Flutter/flutter/fake_store_app/lib/views/product_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatefulWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('상품 목록')),
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
