169f3ef9374e7893ec0e6e424f87174b44aff8b6
BKbud <tsb07040@gmail.com>
2025-02-26 13:18:49 +0900

    detail screen 생성 및 card가 눌렸을 때 detail 화면으로 이동

--- Flutter/flutter/fake_store_app/lib/widgets/product_card.dart
+++ Flutter/flutter/fake_store_app/lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:fake_store_app/models/product.dart';
+import 'package:fake_store_app/views/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
@@ -8,9 +9,18 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 카드가 눌렸을 때 동작 추가 -> InkWell 사용 (onTap 함수를 실행시켜줌)
     return Card(
       margin: const EdgeInsets.all(8),
       child: ListTile(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProductDetailScreen(productId: product.id),
+            ),
+          );
+        },
         leading: Image.network(
           product.image,
           width: 50,
