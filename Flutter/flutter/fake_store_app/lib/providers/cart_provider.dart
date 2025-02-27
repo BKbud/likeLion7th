a17b1900ff53936bc2eb9b3ffeecba18460b28c8
BKbud <tsb07040@gmail.com>
2025-02-27 17:53:47 +0900

    Shared preference를 사용하여 데이터 로컬 저장 기능 구현

--- Flutter/flutter/fake_store_app/lib/providers/cart_provider.dart
+++ Flutter/flutter/fake_store_app/lib/providers/cart_provider.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import '../models/cart_item.dart';
 import '../models/product.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CartProvider with ChangeNotifier {
-  final List<CartItem> _items = [];
+  List<CartItem> _items = [];
+  final String _storageKey = 'cart_data';
+
+  // CartProvider 생성자를 호출할 때 SharedReference에 저장된 장바구니 데이터를 불러오도록 초기화
+  CartProvider() {
+    _loadPrefsToCart();
+  }
 
   List<CartItem> get items => _items;
 
@@ -28,29 +37,69 @@ class CartProvider with ChangeNotifier {
         newQuantity: _items[index].quantity + quantity,
       );
     }
+    _saveCartToPrefs();
     notifyListeners();
   }
 
   void updateQuantity(int productId, int quantity) {
     if (quantity <= 0) {
       removeItem(productId);
+      _saveCartToPrefs();
       return;
     }
 
     final index = _items.indexWhere((item) => item.product.id == productId);
     if (index != -1) {
       _items[index] = _items[index].copyWith(newQuantity: quantity);
+      _saveCartToPrefs();
       notifyListeners();
     }
   }
 
   void removeItem(int productId) {
     _items.removeWhere((item) => item.product.id == productId);
+    _saveCartToPrefs();
     notifyListeners();
   }
 
   void clearCart() {
     _items.clear();
+    _saveCartToPrefs();
     notifyListeners();
+  }
+
+  // 현재의 _items를 JSON 형식으로 역파싱 후 shared references에 저장
+  Future<void> _saveCartToPrefs() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      // items 객체를 List로 변환
+      final List<dynamic> cartDataList =
+          _items.map((item) => item.toJson()).toList();
+      // 변환된 String을 sharedPreference에 저장
+      prefs.setString(_storageKey, jsonEncode(cartDataList));
+    } catch (e) {
+      print('Error: saving cart data ${e}');
+    }
+  }
+
+  Future<void> _loadPrefsToCart() async {
+    try {
+      // preferences로부터 인스턴스를 받아온다. 비동기로 처리하므로 데이터를 받아올 때까지 기다린다.
+      final prefs = await SharedPreferences.getInstance();
+      // 저장소 고유 키값으로 저장된 값을 문자열로 가져온다.
+      final cartDataString = prefs.getString(_storageKey);
+
+      // 데이터가 있는지 확인
+      if (cartDataString != null) {
+        // 데이터가 있으므로 json 데이터를 파싱
+        final List<dynamic> cartDataList =
+            jsonDecode(cartDataString) as List<dynamic>;
+        _items.clear();
+        _items = cartDataList.map((item) => CartItem.fromJson(item)).toList();
+        notifyListeners();
+      }
+    } catch (e) {
+      print('Error loading cart data: ${e}');
+    }
   }
 }
