a81b553afbbf8708295fccf5c828afd6c53b40e1
BKbud <tsb07040@gmail.com>
2025-03-13 15:08:30 +0900

    form & TableView

--- UIKit/basic/HelloWorld/HelloWorld/SecondViewController.swift
+++ UIKit/basic/HelloWorld/HelloWorld/SecondViewController.swift
@@ -6,26 +6,56 @@
 //
 
 import UIKit
- 
- class SecondViewController: UIViewController {
- 
-   override func viewDidLoad() {
-     super.viewDidLoad()
- 
-     setupUI()
-   }
- 
-   func setupUI() {
-     // SecondViewController의 배경색을 노란색으로 설정
-     self.view.backgroundColor = .yellow
- 
-     let label = UILabel()
-     label.text = "Second View"
-     label.textAlignment = .center
-     label.textColor = .black
-     label.font = UIFont.systemFont(ofSize: 24)
-     label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
-     self.view.addSubview(label)
-   }
- 
- }
+
+
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("1. ViewController.viewDidLoad()")
+        
+        setupUI()
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        print("2. ViewController.viewIsAppearing()")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("3. ViewController.viewWillAppear()")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("4. ViewController.viewDidAppear()")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("5 ViewController.viewWillDisappear()")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("6 ViewController.viewDidDisappear()")
+    }
+    
+    
+    func setupUI() {
+        // SecondViewController의 배경색을 노란색으로 설정
+        self.view.backgroundColor = .yellow
+        
+        let label = UILabel()
+        label.text = "Second View"
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 40)
+        self.view.addSubview(label)
+    }
+    
+}
---
+++ UIKit/basic/UIViewDemo/UIViewDemo/FormTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  UITableViewController.swift
+//  UIViewDemo
+//
+//  Created by 정보경 on 3/13/25.
+//
+
+import UIKit
+
+class FormTableViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.separatorColor = .black
+  }
+
+  // MARK: - Table view data source
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 4
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    switch section {
+    case 0:
+      return 1
+    case 3:
+      return 3
+    default:
+      return 2
+    }
+  }
+
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // 테이블 뷰 셀을 재사용 하거나 새로 생성
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+
+    // Configure the cell...
+    var config = UIListContentConfiguration.subtitleCell()
+    config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+    cell.contentConfiguration = config
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    switch indexPath.section {
+    case 0:
+      return 100
+    case 1:
+      return 200
+    default:
+      return 50
+    }
+
+  }
+
+}
+
+#Preview {
+  FormTableViewController(style: .insetGrouped)
+}
