b3ba0fa1f1f5fa9cf6ce463fc667cf83118cc67f
BKbud <tsb07040@gmail.com>
2025-03-21 11:03:39 +0900

    set programmaatic UI

---
+++ UIKit/projects/myTodoList/myTodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTodoList
+//
+//  Created by 정보경 on 3/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
